c79d5ff298e185ab196abe9d9acc49c2af4a1fd7
Abdullah272056 <abdullah272056@gmail.com>
2020-02-13 14:09:40 +0600

    full project

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView=findViewById(R.id.editTextId);
+
+        textView.setText("tthjjdgcd");
+    }
+}
